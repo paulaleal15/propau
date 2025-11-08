f67b533f8d2b1943aa8778a67a6dd7296ea6d2e6
JCMR05 <juancamilomorarangel@gmail.com>
2025-11-07 21:21:52 -0500

    Modificaciones propau

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -2,7 +2,7 @@
 @section('header')
 @endsection
 @section('contenido')
-<form method="GET" action="{{route('web.index')}}">
+<!-- <form method="GET" action="{{route('web.index')}}">
     <div class="container px-4 px-lg-5 mt-4">
         <div class="row">
             <div class="col-md-8 mb-3">
@@ -28,31 +28,31 @@
         </div>
     </div>
 </form>
-<!-- Section-->
-<section class="py-5">
+Section-->
+<!-- <section class="py-5">
     <div class="container px-4 px-lg-5 mt-1">
         <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
             @foreach($productos as $producto)
             <div class="col mb-5">
                 <div class="card h-100">
-                    <!-- Product image-->
-                    <img class="card-img-top" src="{{asset('uploads/productos/'. $producto->imagen) }}"
+                     Product image-->
+                    <!-- <img class="card-img-top" src="{{asset('uploads/productos/'. $producto->imagen) }}"
                         alt="{{$producto->nombre}}" />
-                    <!-- Product details-->
-                    <div class="card-body p-4">
+                     Product details-->
+                    <!-- <div class="card-body p-4">
                         <div class="text-center">
-                            <!-- Product name-->
-                            <h5 class="fw-bolder">{{$producto->nombre}}</h5>
-                            <!-- Product price-->
-                            $ {{number_format($producto->precio, 2)}}
-                        </div>
+                            <! Product name-->
+                            <!-- <h5 class="fw-bolder">{{$producto->nombre}}</h5>
+                             Product price-->
+                            <!-- $ {{number_format($producto->precio, 2)}} -->
+                        </div> 
                     </div>
-                    <!-- Product actions-->
-                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                    <!-- Product actions -->
+                    <!-- <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
                         <div class="text-center"><a class="btn btn-outline-dark mt-auto"
                                 href="{{route('web.show', $producto->id)}}">Ver
-                                producto</a></div>
-                    </div>
+                                producto</a></div> -->
+                    <!-- </div>
                 </div>
             </div>
             @endforeach
@@ -60,7 +60,7 @@
         <div class="card-footer clearfix">
             {{ $productos->appends(['search' => request('search'), 'sort' => request('sort')])->links() }}
         </div>
+    </div> -->
     </div>
-    </div>
-</section>
+</section>  
 @endsection
--- resources/views/web/partials/header.blade.php
+++ resources/views/web/partials/header.blade.php
@@ -1,8 +1,17 @@
-<header class="bg-dark py-5">
+<header class="bg-dark ">
     <div class="container px-4 px-lg-5 my-5">
-        <div class="text-center text-white">
+        <div class="bg-imperial bg-over">
             <h1 class="display-4 fw-bolder">Imperial Suites</h1>
+            
             <p class="lead fw-normal text-white mb-0">Gestion Hotelera</p>
+<br>
+                <a href="{{route('carrito.mostrar')}}" class="btn btn-outline-dark">
+                <i class=""></i>
+                Reservar una Habitacion 
+            </a>
+            
+
         </div>
     </div>
+   
 </header>
