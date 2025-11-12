4ab279483281111aa698e5e966ccd5237732dedf
paulaleal15 <paulaandrealc@ufps.edu.co>
2025-11-11 21:11:56 -0500

    modificaciones

--- resources/views/web/partials/header.blade.php
+++ resources/views/web/partials/header.blade.php
@@ -1,17 +1,13 @@
-<header class="bg-dark ">
-    <div class="container px-4 px-lg-5 my-5">
-        <div class="bg-imperial bg-over">
-            <h1 class="display-4 fw-bolder">Imperial Suites</h1>
-            
-            <p class="lead fw-normal text-white mb-0">Gestion Hotelera</p>
-<br>
-                <a href="{{route('carrito.mostrar')}}" class="btn btn-outline-dark">
-                <i class=""></i>
-                Reservar una Habitacion 
-            </a>
-            
-
-        </div>
+<header class="bg-dark d-flex justify-content-center align-items-center">
+  <div class="container-fluid text-center">
+    <div class="bg-imperial bg-over">
+      <h1 class="display-4 fw-bolder">Imperial Suites</h1>
+      <p class="lead fw-normal text-white mb-0">Gestión Hotelera</p>
+      <br>
+      <a href="{{ route('carrito.mostrar') }}" class="btn btn-reserva">
+         Reservar una Habitación
+    </a>
     </div>
-   
+  </div>
 </header>
+
